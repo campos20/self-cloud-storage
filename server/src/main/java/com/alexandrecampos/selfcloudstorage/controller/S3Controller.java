b3970b4fd82b7d45a24b29398f098add0574ca6a
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2022-05-21 20:58:06 -0300

    Downgrade to spring 2.6.8 so we can use openapi, add openapi

--- server/src/main/java/com/alexandrecampos/selfcloudstorage/controller/S3Controller.java
+++ server/src/main/java/com/alexandrecampos/selfcloudstorage/controller/S3Controller.java
@@ -1,9 +1,13 @@
 package com.alexandrecampos.selfcloudstorage.controller;
 
 import com.amazonaws.services.s3.model.Bucket;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+@RequestMapping("s3")
 public interface S3Controller {
+    @GetMapping
     List<Bucket> listBuckets();
 }

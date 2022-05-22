30956004220d1360853df084c5c028834059f6cd
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2022-05-22 10:16:56 -0300

    Correctly display antd gutter

--- client/src/main/component/BucketsComponent.tsx
+++ client/src/main/component/BucketsComponent.tsx
@@ -7,26 +7,31 @@ import style from "./BucketsComponent.module.css";
 
 export const BucketsComponent = () => {
   const [buckets, setBuckets] = useState<Bucket[]>([]);
-  const [screenWidth, setWidth] = useState(window.innerWidth);
   const [selected, setSelected] = useState<number>();
 
   const fetchBuckets = useCallback(() => {
     s3Api.listBuckets().then((response) => setBuckets(response.data));
   }, []);
 
-  useEffect(fetchBuckets, [fetchBuckets]);
-
   useEffect(() => {
-    const debouncedHandleResize = debounce(() => {
-      setWidth(window.innerWidth);
-    }, 500);
+    const handleKeyPress = (e: KeyboardEvent) => {
+      if (e.key === "ArrowRight") {
+        setSelected((o) =>
+          o == null ? 0 : Math.min(o + 1, buckets.length - 1)
+        );
+      } else if (e.key === "ArrowLeft") {
+        setSelected((o) => (o == null ? 0 : Math.max(o - 1, 0)));
+      }
+    };
 
-    window.addEventListener("resize", debouncedHandleResize);
+    window.addEventListener("keydown", handleKeyPress);
 
     return () => {
-      window.removeEventListener("resize", debouncedHandleResize);
+      window.removeEventListener("keydown", handleKeyPress);
     };
-  }, []);
+  }, [buckets]);
+
+  useEffect(fetchBuckets, [fetchBuckets]);
 
   return (
     <>
@@ -34,7 +39,13 @@ export const BucketsComponent = () => {
       <Row gutter={[8, 8]}>
         {buckets.length > 0 &&
           buckets.map((bucket, i) => (
-            <Col key={bucket.name} span={4} onClick={() => setSelected(i)}>
+            <Col
+              key={bucket.name}
+              onClick={() => setSelected(i)}
+              xs={{ span: 24 }}
+              sm={{ span: 12 }}
+              md={{ span: 6 }}
+            >
               <div
                 className={
                   style.bucket +

4dd8c25b0911fbdf4c9a8108930cf935fc56d877
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2022-05-22 16:10:37 -0300

    Mock s3 objects

--- server/src/main/java/com/alexandrecampos/selfcloudstorage/service/impl/S3ServiceImpl.java
+++ server/src/main/java/com/alexandrecampos/selfcloudstorage/service/impl/S3ServiceImpl.java
@@ -1,37 +1,52 @@
 package com.alexandrecampos.selfcloudstorage.service.impl;
 
 import com.alexandrecampos.selfcloudstorage.service.S3Service;
+import com.alexandrecampos.selfcloudstorage.util.LoadResourceUtil;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.Bucket;
-import com.amazonaws.services.s3.model.ListObjectsV2Result;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class S3ServiceImpl implements S3Service {
     private final AmazonS3 amazonS3;
-//    private final ObjectMapper objectMapper;
-//    private static final TypeReference BUCKET_TYPE_REFERENCE = new TypeReference<List<Bucket>>() {
-//    };
+    private final ObjectMapper objectMapper;
+    private static final TypeReference BUCKET_TYPE_REFERENCE = new TypeReference<List<Bucket>>() {
+    };
 
     @Override
     public List<Bucket> listBuckets() {
-        return amazonS3.listBuckets();
-//        String mock = LoadResourceUtil.getResource("mock/bucketList.json");
-//        try {
-//            return (List<Bucket>) objectMapper.readValue(mock, BUCKET_TYPE_REFERENCE);
-//        } catch (JsonProcessingException e) {
-//            throw new RuntimeException(e);
-//        }
+//        return amazonS3.listBuckets();
+        String mock = LoadResourceUtil.getResource("mock/bucketList.json");
+        try {
+            return (List<Bucket>) objectMapper.readValue(mock, BUCKET_TYPE_REFERENCE);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public List<S3ObjectSummary> listObjects(String bucketName) {
-        ListObjectsV2Result objects = amazonS3.listObjectsV2(bucketName);
-        return objects.getObjectSummaries();
+//        ListObjectsV2Result objects = amazonS3.listObjectsV2(bucketName);
+//        return objects.getObjectSummaries();
+        List<S3ObjectSummary> objects = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            S3ObjectSummary object = new S3ObjectSummary();
+            object.setBucketName("example-bucket");
+            object.setKey("docs/file/file_" + i + ".txt");
+            object.setSize(18434 + i);
+            object.setStorageClass("STANDARD");
+            object.setETag("123456789" + i);
+            objects.add(object);
+        }
+        return objects;
     }
 }

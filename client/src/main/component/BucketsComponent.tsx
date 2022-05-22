6209706fb9616889311e8939d824ffbae57b5f70
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2022-05-22 09:29:33 -0300

    Introduce banner and bucket mock

--- client/src/main/component/BucketsComponent.tsx
+++ client/src/main/component/BucketsComponent.tsx
@@ -1,12 +1,14 @@
-import { Card } from "antd";
-import { debounce } from "lodash";
+import { Col, Row } from "antd";
+import { chunk, debounce } from "lodash";
 import { useCallback, useEffect, useState } from "react";
 import { s3Api } from "../api/S3Api";
 import { Bucket } from "../model/Bucket";
 
+import style from "./BucketsComponent.module.css";
+
 export const BucketsComponent = () => {
   const [buckets, setBuckets] = useState<Bucket[]>([]);
-  const [screenWidth, setWidth] = useState(window.innerHeight);
+  const [screenWidth, setWidth] = useState(window.innerWidth);
 
   const fetchBuckets = useCallback(() => {
     s3Api.listBuckets().then((response) => setBuckets(response.data));
@@ -17,7 +19,7 @@ export const BucketsComponent = () => {
   useEffect(() => {
     const debouncedHandleResize = debounce(() => {
       setWidth(window.innerWidth);
-    }, 500);
+    }, 1);
 
     window.addEventListener("resize", debouncedHandleResize);
 
@@ -26,34 +28,33 @@ export const BucketsComponent = () => {
     };
   }, []);
 
-  const getWidth = () => {
+  const getColumns = () => {
     if (screenWidth > 992) {
-      return 4;
+      return 6;
     }
     if (screenWidth > 576) {
-      return 2;
+      return 4;
     }
-    return 1;
+    return 2;
   };
 
-  const width = getWidth();
+  const columns = getColumns();
+
+  const chunks = chunk(buckets, columns);
 
   return (
     <>
       <h2>Folders</h2>
-      {buckets.length > 0 && (
-        <Card>
-          {buckets.map((b) => (
-            <Card.Grid
-              key={b.name}
-              style={{ textAlign: "center", width: `${100 / width}%` }}
-              hoverable
-            >
-              {b.name}
-            </Card.Grid>
-          ))}
-        </Card>
-      )}
+      {buckets.length > 0 &&
+        chunks.map((c) => (
+          <Row gutter={[16, 16]}>
+            {c.map((bucket) => (
+              <Col className={style.bucket} span={24 / columns}>
+                {bucket.name}
+              </Col>
+            ))}
+          </Row>
+        ))}
     </>
   );
 };
--- server/src/main/java/com/alexandrecampos/selfcloudstorage/service/S3ServiceImpl.java
+++ server/src/main/java/com/alexandrecampos/selfcloudstorage/service/S3ServiceImpl.java
@@ -1,7 +1,11 @@
 package com.alexandrecampos.selfcloudstorage.service;
 
+import com.alexandrecampos.selfcloudstorage.util.LoadResourceUtil;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.Bucket;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,9 +15,18 @@ import java.util.List;
 @AllArgsConstructor
 public class S3ServiceImpl implements S3Service {
     private final AmazonS3 amazonS3;
+    private final ObjectMapper objectMapper;
+
+    private static final TypeReference BUCKET_TYPE_REFERENCE = new TypeReference<List<Bucket>>() {
+    };
 
     @Override
     public List<Bucket> listBuckets() {
-        return amazonS3.listBuckets();
+        String mock = LoadResourceUtil.getResource("mock/bucketList.json");
+        try {
+            return (List<Bucket>) objectMapper.readValue(mock, BUCKET_TYPE_REFERENCE);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
